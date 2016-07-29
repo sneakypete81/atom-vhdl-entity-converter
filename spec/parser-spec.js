648fb7973ec69a735f13a35224f7883d76d94f47
sneakypete81 <sneakypete81@gmail.com>
2016-07-29 21:53:18 +0100

    Make generic matching case insensitive (#2)

--- spec/parser-spec.js
+++ spec/parser-spec.js
@@ -2,6 +2,12 @@
 import { Parser } from "../lib/parser"
 import { loadFixture } from "./helpers"
 
+const adderGenerics = [{name: "WIDTH", type: "integer", default: "3"},
+                       {name: "HEIGHT", type: "integer", default: "2"}]
+
+const adderWithCaseGenerics = [{name: 'wiDTH', type: 'Integer', default: '3'},
+                               {name: 'HEight', type: 'inTeger', default: '2' }]
+
 describe("parser.parseEntityName", function() {
   it("can parse an entity name", function() {
     entity = new Parser(loadFixture("entity/adder.vhd"))
@@ -11,5 +17,17 @@ describe("parser.parseEntityName", function() {
   it("can parse an entity name with mixed case", function() {
     entity = new Parser(loadFixture("entity/adder_with_case.vhd"))
     expect(entity.name).toEqual("addWithCase")
+  })
+})
+
+describe("parser.parseGenerics", function() {
+  it("can parse generics from an entity", function() {
+    entity = new Parser(loadFixture("entity/adder.vhd"))
+    expect(entity.generics).toEqual(adderGenerics)
+  })
+
+  it("can parse generics from an entity with mixed case", function() {
+    entity = new Parser(loadFixture("entity/adder_with_case.vhd"))
+    expect(entity.generics).toEqual(adderWithCaseGenerics)
   })
 })
